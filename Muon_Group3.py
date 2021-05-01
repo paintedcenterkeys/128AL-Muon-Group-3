0edc50a82611df862fef66501e23cdfe4edf9511
sarahalexw <webster.sarah.a@gmail.com>
2021-05-01 01:23:54 -0700

    comments and alternative t_prime

--- Muon_Group3.py
+++ Muon_Group3.py
@@ -1,25 +1,72 @@
-print('Hello World')
-print('Hello I made a change -Sarah')
-
-print('mason test')
-
-print('Hello')
-
-
-
 import numpy as np
-import matplotlib.pyplot as plt
 
-x = np.linspace(0, 10, 1000)
-m = 1
+#Constants: Not subject to change
+c=3e8 #Speed of light, m/s
+m=1.88e-28/1.6605e-27 #mass of muon, AU
+p0=101325 #sea level standard atmospheric pressure, Pa
+T0= 288.15 #sea level standard temperature, K
+g= 9.807 #m/s^2
+L= .0065 #temperature lapse rate, K/m
+R= 8.31446 #ideal gas constant, J/(mol K)
+M= 0.0289652 #molar mass of dry air, kg/mol
+e = -1.602e-19 #electron charge, e
 
-def y(x, m):
-    return x*m
+#Constants: Subject to change based on info we get from previous groups
+A=13 #atomic weight of the stopping medium
+Z=7 #atomic number of N
+I=A*Z #approximation of mean excitation energy of the stopping medium of atoms
+z=1*e #charge of muon relative to electron charge
 
-plt.plot(x, y(x, m))
+def get_C0(x,gamma,Beta):
+    """Given the current height from the ground, gamma, and beta, returns the updated C0 value
+    C0 is defined as the right side of the Bethe formula divided by rho"""
+    C0=0.3071*Z*z*z/(A*Beta*Beta)*(np.log(2*m*c*c*Beta*Beta*gamma*gamma/I)-Beta*Beta)
+    return C0
 
+def get_rho(x):
+    """Given the height from the ground, returns the mass dencity, rho of the air at that height."""
+    rho=p0*M/(R*T0)*(1-L*x/T0)**(g*M/(R*L)-1)
+    return rho
 
-print('mason test')
+#Hey group members can yall check which of these get_t_prime functions is right????? - thx
+
+def alternate_get_t_prime(C0, rho, gamma1, gamma2):
+    """Given the updated C0 and rho, the previous gamma, and the updated gamma, returns the updated t_prime
+    t_prime is the time elapsed in the muon's reference frame"""
+    t_prime = m*c/(rho*C0)*(gamma1 * (gamma1**2 - 1)**(-3/2) - gamma2 * (gamma2**2 - 1)**(-3/2))
+    return t_prime
+
+def get_t_prime(C0,rho,gamma1,gamma2):
+    t_prime=m*c/(rho*C0)*(1/np.sqrt(gamma2**2-1)-1/np.sqrt(gamma1**2-1))*(gamma2-gamma1)
+    return t_prime
+    
+
+#Initial Conditions, will be provided from the previous groups as a list for each condition of all the muons
+Beta=.1 #dummy variable amount, v/c
+x0=12000 #height of troposphere
+gamma=1/np.sqrt(1-Beta*Beta) #Lorentz factor
+E=gamma*m*c**2 #double check, not correct
+
+deltax=1 #dummy amount, will change
+
+for i,x in enumerate(range(x0,0,-deltax)):
+    C0=get_C0(x,gamma,Beta)
+    rho=get_rho(x)
+    dE=C0*rho*deltax
+    E1 = E
+    E2 = E1 - dE #feel like this should be adding dE but that results in increasing energy
+    gamma1 = gamma
+    #gamma2 = E/(m*c**2)
+    gamma2=E2*gamma1/E1
+    Beta1=Beta
+    Beta2=np.sqrt(1-1/(gamma2*gamma2))
+    t_prime=get_t_prime(C0,rho,gamma1,gamma2)
+
+    gamma = gamma2
+    Beta = Beta2
+    E = E2
+    if x%500==0:
+        print(x,Beta,E,C0,rho,gamma,t_prime,'\n')
 
 
 
