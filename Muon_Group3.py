704007a86774a9a46a41ef062b014f8bf635fd3e
paintedcenterkeys <paintedcenterkeys@gmail.com>
2021-05-02 18:06:54 -0700

    Add files via upload
    the reworked code that uses numpy instead of python for loop

--- Muon_Group3.py
+++ Muon_Group3.py
@@ -19,8 +19,8 @@ Z=7 #atomic number of N
 I=A*Z #approximation of mean excitation energy of the stopping medium of atoms
 z=1*e #charge of muon relative to electron charge
 
-def get_C0(x,gamma,Beta):
-    """Given the current height from the ground, gamma, and beta, returns the updated C0 value
+def get_C0(gamma,Beta):
+    """Given the current gamma and beta, returns the updated C0 value
     C0 is defined as the right side of the Bethe formula divided by rho"""
     C0=0.3071*Z*z*z/(A*Beta*Beta)*(np.log(2*m*c*c*Beta*Beta*gamma*gamma/I)-Beta*Beta)
     return C0
@@ -32,67 +32,111 @@ def get_rho(x):
 
 #Hey group members can yall check which of these get_t_prime functions is right????? - thx
 
-def alternate_get_t_prime(C0, rho, gamma1, gamma2):
-    """Given the updated C0 and rho, the previous gamma, and the updated gamma, returns the updated t_prime
-    t_prime is the time elapsed in the muon's reference frame"""
-    t_prime = m*c/(rho*C0)*(gamma1 * (gamma1**2 - 1)**(-3/2) - gamma2 * (gamma2**2 - 1)**(-3/2))
-    return t_prime
+# def alternate_get_t_prime(C0, rho, gamma1, gamma2):
+#     """Given the updated C0 and rho, the previous gamma, and the updated gamma, returns the updated t_prime
+#     t_prime is the time elapsed in the muon's reference frame"""
+#     t_prime = m*c/(rho*C0)*(gamma1 * (gamma1**2 - 1)**(-3/2) - gamma2 * (gamma2**2 - 1)**(-3/2))
+#     return t_prime
 
-def get_t_prime(C0,rho,gamma1,gamma2):
-    t_prime=m*c/(rho*C0)*(1/np.sqrt(gamma2**2-1)-1/np.sqrt(gamma1**2-1))*(gamma2-gamma1)
-    return t_prime
+# def get_t_prime(C0,rho,gamma1,gamma2):
+#     t_prime=m*c/(rho*C0)*(1/np.sqrt(gamma2**2-1)-1/np.sqrt(gamma1**2-1))*(gamma2-gamma1)
+#     return t_prime
+
+
+# we're just doing riemann sum here, I think. So it should be the value of the expression, times dgamma
+def get_dt_prime(C0,rho,gamma1,gamma2):
+	gamma_average = (gamma2+gamma1)/2.
+	dg = gamma1-gamma2
+	print(C0)
+	dt_prime = (m*c/(rho*C0)) * dg * 1./(np.sqrt(gamma_average**2-1))
+	return dt_prime
+
+def findDecayProbability(t_prime):
+    """Given the the time it takes for the muon to reach the detector in its own reference frame, returns the probability that it decays. """
+    tau = 2.2*10**-6 #seconds
+    return np.exp(-1*t_prime/tau)
     
 
 #Initial Conditions, will be provided from the previous groups as a list for each condition of all the muons
-Beta_initial=[.1,.1] #dummy variable amount, v/c
-x0_initial=[12000,10000] #height of troposphere
-gamma_initial = [] #Lorenz Factor
-for j in range(len(Beta_initial)):
-    gamma_initial.append(1/np.sqrt(1-Beta_initial[j]*Beta_initial[j]))
-E_initial=[] #double check, not correct
-for k in range(len(x0_initial)):
-    E_initial.append(gamma_initial[k]*m*c**2)
-t_prime = 0
+E_initial = np.array([1e18,1e20], dtype = 'f')
+x0_flat_initial = np.array([12000,10000], dtype = 'f') #height of troposphere
+x0_round_initial = np.array([15000,13000], dtype = 'f')
 
-deltax=1 #dummy amount, will change
+#instead of appending, it makes the list of intial gammas by using numpy. A litte faster - mason
+gamma_initial = E_initial/(m*c**2)
 
-Beta_final = []
-E_final = []
-C0_final = []
-rho_final = []
-gamma_final = []
-t_prime_final = []
+#same thing here
+Beta_initial = np.sqrt(1-1/(gamma_initial**2))
 
-for muon in range(len(x0_initial)):
-    Beta = Beta_initial[muon]
-    x0 = x0_initial[muon]
-    gamma = gamma_initial[muon]
-    E = E_initial[muon]
-    for i,x in enumerate(range(x0,0,-deltax)):
-        C0=get_C0(x,gamma,Beta)
-        rho=get_rho(x)
-        dE=C0*rho*deltax
-        E1 = E
-        E2 = E1 - dE #feel like this should be adding dE but that results in increasing energy
-        gamma1 = gamma
-        gamma2 = E/(m*c**2)
-        #gamma2=E2*gamma1/E1
-        Beta1=Beta
-        Beta2=np.sqrt(1-1/(gamma2*gamma2))
-        t_prime =+ get_t_prime(C0,rho,gamma1,gamma2)
+t_prime_flat = 0
+t_prime_round = 0
 
-        gamma = gamma2
-        Beta = Beta2
-        E = E2
-        if x%500==0:
-            print(x,Beta,E,C0,rho,gamma,t_prime,'\n')
-    Beta_final.append(Beta)
-    E_final.append(E)
-    C0_final.append(C0)
-    rho_final.append(rho)
-    gamma_final.append(gamma)
-    t_prime_final.append(t_prime)
 
+E_flat_final = np.zeros(len(x0_flat_initial))
+E_round_final = np.zeros(len(x0_flat_initial))
+t_prime_flat_final = np.zeros(len(x0_round_initial))
+t_prime_round_final = np.zeros(len(x0_round_initial))
+
+number_of_steps = 1000. #dummy amount, will change
+dx_flat = x0_flat_initial/number_of_steps
+dx_round = x0_round_initial/number_of_steps
+
+#Find final energies and t_prime for flat earth
+Beta = Beta_initial
+x = x0_flat_initial
+gamma = gamma_initial
+E = E_initial
+
+for i in range(int(number_of_steps)):
+    C0=get_C0(gamma,Beta)
+    rho=get_rho(x)
+    dE=C0*rho*dx_flat
+    E1 = E
+    E2 = E1 - dE #feel like this should be adding dE but that results in increasing energy
+    gamma1 = gamma
+    gamma2 = E2/(m*c**2)
+    Beta1=Beta
+    Beta2=np.sqrt(1-1/(gamma2*gamma2))
+    t_prime_flat += get_dt_prime(C0,rho,gamma1,gamma2)
+
+    gamma = gamma2
+    Beta = Beta2
+    E = E2
+    x += dx_flat
+
+E_flat_final = E
+t_prime_flat_final = t_prime_flat
+
+
+#Find the final energies and t_prime for round earth
+Beta = Beta_initial
+x = x0_round_initial
+gamma = gamma_initial
+E = E_initial
+
+for i in range(int(number_of_steps)):
+    C0=get_C0(gamma,Beta)
+    rho=get_rho(x)
+    dE=C0*rho*dx_round
+    E1 = E
+    E2 = E1 - dE #feel like this should be adding dE but that results in increasing energy
+    gamma1 = gamma
+    gamma2 = E2/(m*c**2)
+    Beta1=Beta
+    Beta2=np.sqrt(1-1/(gamma2*gamma2))
+    t_prime_round += get_dt_prime(C0,rho,gamma1,gamma2)
+
+    gamma = gamma2
+    Beta = Beta2
+    E = E2
+    x += dx_round
+
+E_round_final = E
+t_prime_round_final = t_prime_round
+ 
+
+print(t_prime_round)
+print(t_prime_flat)
 
 #Simulating how many muons hit the detector:
     #Checks for muons in the proper energy range
@@ -101,15 +145,11 @@ for muon in range(len(x0_initial)):
         
 energyDetected = 160 #Mev
 energyAllowance = 2 #Mev
-tau = 2.2*10**-6 #seconds
-
-def findDecayProbability(t_prime):
-    return np.exp(-1*t_prime/tau)
  
 anglesOfDetectedMuons_flatEarth = []
 anglesOfDetectedMuons_roundEarth = []
 
-for muon in range(len(x0_initial)):
+for muon in range(len(x0_flat_initial)):
     #Check detection for flat Earth
     if (energyDetected - energyAllowance <= E_flat_final[muon] <= energyDetected + energyAllowance):
         #Decide if it decayed
